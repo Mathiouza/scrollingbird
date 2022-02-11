b27ebf0764466ebdcaedfaa1ec4cffd83b8d35a8
mathiouza <mathiouza@live.fr>
2022-02-10 21:55:14 -0500

    Add node js server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use("/", (req, res) => {
+    res.sendFile(__dirname+'index.html');
+});
+
+app.listen(8000, () => {
+    console.log("ScrollingBird listening to localhost:8000");
+});
