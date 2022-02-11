9b45743e76a56b84101972d4d47a3cee371f5f1a
mathiouza <mathiouza@live.fr>
2022-02-10 22:04:26 -0500

    clean folder architecture

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 app.use("/", (req, res) => {
-    res.sendFile(__dirname+'index.html');
+    res.sendFile(__dirname+'/index.html');
 });
 
 app.listen(8000, () => {
