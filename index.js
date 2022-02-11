6a70233110b2921fdad785d545e1fd28f94cd113
Mathiouza <mathiouza@live.fr>
2022-02-11 20:45:24 +0000

    deployment

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+console.log(__dirname+"/public");
+
 app.use("/", (req, res) => {
     res.sendFile(__dirname+'/index.html');
 });
 
-app.listen(8000, () => {
-    console.log("ScrollingBird listening to localhost:8000");
+app.listen(8001, () => {
+    console.log("ScrollingBird listening to localhost:8001");
 });
